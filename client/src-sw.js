3b8d5009c7c1a5d05f134112f47722c30b09bf7f
fardinatabassum <fardinatabassum@gmail.com>
2023-05-08 22:35:09 -0400

    fixed src to cache css properly

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,15 +1,15 @@
 // const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const {warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -18,15 +18,12 @@ const pageCache = new CacheFirst({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
-  
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
-
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // const matchCallback = ({ request }) => {
 //   console.log(request);
@@ -37,12 +34,13 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 //     request.destination === 'script'
 //   );
 // };
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 // TODO: Implement asset caching
 registerRoute(
   // matchCallback,
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
+    cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
